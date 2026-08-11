42a69bcedbf39798a73767b9ebd99388a9570040
Law1it <suyogtuladhar04@gmail.com>
2026-08-11 20:50:43 +0545

    QuerySelectorAll and for loop

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,9 @@
 console.log("Manga Translator content script loaded.");
+
+const images = document.querySelectorAll("img");
+
+console.log(`Found ${images.length} images on this page.`);
+
+for (let i = 0; i < images.length; i++) {
+    console.log(`Image ${i}:`, images[i]);
+}
