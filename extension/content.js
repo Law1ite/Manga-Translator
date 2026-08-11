fbdbc1d53ed99a81a591423d70c5152580407e64
Law1it <suyogtuladhar04@gmail.com>
2026-08-11 23:05:06 +0545

    QuerySelectorAll and for loop

--- extension/content.js
+++ extension/content.js
@@ -5,5 +5,9 @@ const images = document.querySelectorAll("img");
 console.log(`Found ${images.length} images on this page.`);
 
 for (let i = 0; i < images.length; i++) {
-    console.log(`Image ${i}:`, images[i]);
+    console.log(`Image ${i}:`);
+    console.log("URL:", images[i].src);
+    console.log("Width:", images[i].width);
+    console.log("Height:", images[i].height);
+    console.log("Alt text:", images[i].alt);
 }
