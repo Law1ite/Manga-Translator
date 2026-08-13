08f8efeb9475b612d5a02791fab8643b83195048
Law1it <suyogtuladhar04@gmail.com>
2026-08-13 10:48:59 +0545

    Get Bounding Client Rect

--- extension/content.js
+++ extension/content.js
@@ -5,21 +5,27 @@ const images = document.querySelectorAll("img");
 console.log(`Found ${images.length} images on this page.`);
 
 for (let i = 0; i < images.length; i++) {
-    console.log(`Image ${i}:`);
+    console.log("Image", i);
+
     console.log("URL:", images[i].src);
     console.log("Width:", images[i].width);
     console.log("Height:", images[i].height);
-    console.log("Alt text:", images[i].alt);
+    console.log("Natural width:", images[i].naturalWidth);
+    console.log("Natural height:", images[i].naturalHeight);
 
     const aspectRatio = images[i].width / images[i].height;
     console.log("Aspect ratio:", aspectRatio);
 
-    if (
-    images[i].width > 500 &&
-    images[i].height > 500 &&
-    aspectRatio >= 0.4 &&
-    aspectRatio <= 1.0
-) {
-    console.log("Possible manga image:", images[i]);
-}
+    const rect = images[i].getBoundingClientRect();
+
+    console.log("Position:", rect.x, rect.y);
+    console.log("Displayed size:", rect.width, rect.height);
+    console.log("Top:", rect.top);
+    console.log("Bottom:", rect.bottom);
+
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+        console.log("VISIBLE");
+    } else {
+        console.log("NOT VISIBLE");
+    }
 }
